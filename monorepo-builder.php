ffc2dcea4e0026ac32f4a3cc8e6c430e9058c03e
Jordi <jordism91@gmail.com>
2021-08-06 12:00:02 +0200

    prepare release

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -20,7 +20,6 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 use Symplify\MonorepoBuilder\ValueObject\Option;
 
@@ -46,10 +45,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     ]);
 
     $services->set(UpdateReplaceReleaseWorker::class);
+    $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
     $services->set(ComposerNormalizePreReleaseWorker::class);
     $services->set(TagVersionReleaseWorker::class);
     $services->set(PushTagReleaseWorker::class);
-    $services->set(UpdateBranchAliasReleaseWorker::class);
+    $services->set(SetNextMutualDependenciesReleaseWorker::class);
     $services->set(ComposerNormalizePostReleaseWorker::class);
     $services->set(PushNextDevReleaseWorker::class);
 };
