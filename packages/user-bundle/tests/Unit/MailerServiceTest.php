9e9f4f2c1dd9c67691206cc1e04f6fd67ae911f9
Aitor Errasti <aitor@runroom.com>
2021-09-30 17:07:24 +0200

    Added mailer service test

---
+++ packages/user-bundle/tests/Unit/MailerServiceTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Runroom package.
+ *
+ * (c) Runroom <runroom@runroom.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Runroom\UserBundle\Tests\Unit;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Runroom\UserBundle\Entity\User;
+use Runroom\UserBundle\Service\MailerService;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordToken;
+
+class MailerServiceTest extends TestCase
+{
+    /** @var MockObject&MailerInterface */
+    private $mailer;
+    /** @var MockObject&TranslatorInterface */
+    private $translator;
+
+    private MailerService $service;
+
+    private string $fromEmail;
+    private string $fromName;
+
+    protected function setUp(): void
+    {
+        $this->mailer = $this->createMock(MailerInterface::class);
+        $this->translator = $this->createMock(TranslatorInterface::class);
+        $this->fromEmail = 'user@email.com';
+        $this->fromName = 'user';
+
+        $this->service = new MailerService(
+            $this->mailer,
+            $this->translator,
+            $this->fromEmail,
+            $this->fromName
+        );
+    }
+
+    /** @test */
+    public function itCallsMailerWhenUserHasEmail(): void
+    {
+        $user = new User();
+        $user->setEmail('user@email.com');
+        $resetPasswordToken = new ResetPasswordToken(
+            'token',
+            new \DateTime()
+        );
+
+        $this->translator->method('trans')->with('email.subject')->willReturn('Subject');
+
+        $this->mailer->expects(static::once())
+            ->method('send');
+
+        $this->service->sendResetPasswordEmail($user, $resetPasswordToken);
+    }
+
+    /** @test */
+    public function itDoesntCallMailerWhenUserDoesntHaveEmail(): void
+    {
+        $user = new User();
+        $resetPasswordToken = new ResetPasswordToken(
+            'token',
+            new \DateTime()
+        );
+
+        $this->translator->method('trans')->with('email.subject')->willReturn('Subject');
+
+        $this->mailer->expects(static::never())
+            ->method('send');
+
+        $this->service->sendResetPasswordEmail($user, $resetPasswordToken);
+    }
+}
