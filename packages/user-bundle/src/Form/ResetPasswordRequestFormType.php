702cea690bfae6157a708f8fd42a1196733b9b95
Jordi <jordism91@gmail.com>
2021-10-07 15:10:42 +0200

    fix cs fixer

--- packages/user-bundle/src/Form/ResetPasswordRequestFormType.php
+++ packages/user-bundle/src/Form/ResetPasswordRequestFormType.php
@@ -17,7 +17,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 final class ResetPasswordRequestFormType extends AbstractType
