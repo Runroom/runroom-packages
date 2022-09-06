047d81eb9b92c6b9c9b06bbc28d2a0b537dd3016
a.dmitryuk <a.dmitryuk@movavi.com>
2022-09-06 20:28:20 +0700

    Licence

--- packages/sortable-behavior-bundle/src/Admin/SortableAdminTrait.php
+++ packages/sortable-behavior-bundle/src/Admin/SortableAdminTrait.php
@@ -6,6 +6,15 @@ namespace Runroom\SortableBehaviorBundle\Admin;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Route\RouteCollectionInterface;
 
+/*
+ * This file is part of the Runroom package.
+ *
+ * (c) Runroom <runroom@runroom.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 trait SortableAdminTrait
 {
     /**
