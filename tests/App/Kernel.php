68031e5ad9fd9c3a2fa299423c08f1ceca8a1e6a
Jordi Sala Morales <jordism91@gmail.com>
2022-09-06 18:00:08 +0200

    fix logs and build on 7.4

--- tests/App/Kernel.php
+++ tests/App/Kernel.php
@@ -43,13 +43,13 @@ use Sonata\Form\Bridge\Symfony\SonataFormBundle;
 use Sonata\MediaBundle\Model\GalleryItemInterface;
 use Sonata\MediaBundle\SonataMediaBundle;
 use Sonata\Twig\Bridge\Symfony\SonataTwigBundle;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Bundle\SecurityBundle\SecurityBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorageFactory;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 use Symfony\Component\Security\Http\Authentication\AuthenticatorManager;
@@ -125,7 +125,7 @@ final class Kernel extends BaseKernel
             ],
         ];
 
-        if (class_exists(NativeSessionStorageFactory::class)) {
+        if (method_exists(AbstractController::class, 'renderForm')) {
             $frameworkConfig['session'] = ['storage_factory_id' => 'session.storage.factory.mock_file'];
         } else {
             $frameworkConfig['session'] = ['storage_id' => 'session.storage.mock_file'];
