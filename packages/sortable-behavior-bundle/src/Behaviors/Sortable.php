633419a5c71715e77dace50aee92ba27cf28feb0
Jordi Sala Morales <jordism91@gmail.com>
2023-04-13 10:05:20 +0200

    Update Sortable.php

--- packages/sortable-behavior-bundle/src/Behaviors/Sortable.php
+++ packages/sortable-behavior-bundle/src/Behaviors/Sortable.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Runroom\SortableBehaviorBundle\Behaviors;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 
