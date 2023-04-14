0c3fa976ab2466d93d0ce34cccb0a5bd7b895af5
Jordi Sala Morales <jordism91@gmail.com>
2023-04-14 13:26:10 +0000

    Fix psalm

--- packages/user-bundle/tests/Unit/UserManipulatorTest.php
+++ packages/user-bundle/tests/Unit/UserManipulatorTest.php
@@ -59,7 +59,7 @@ class UserManipulatorTest extends TestCase
             $this->passwordHasher = $this->getMockBuilder(UserPasswordHasherInterface::class)->getMock();
         } else {
             /**
-             * @psalm-suppress PropertyTypeCoercion UndefinedDocblockClass
+             * @psalm-suppress PropertyTypeCoercion
              */
             $this->passwordEncoder = $this->createMock(UserPasswordEncoderInterface::class);
         }
@@ -95,6 +95,9 @@ class UserManipulatorTest extends TestCase
                 ->with($user, 'new_password')
                 ->willReturn('hashed_password');
         } elseif (null !== $this->passwordEncoder) {
+            /**
+             * @psalm-suppress UndefinedDocblockClass
+             */
             $this->passwordEncoder->expects(static::once())
                 ->method('encodePassword')
                 ->with($user, 'new_password')
